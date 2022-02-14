c53f5bab444ab6fe56abe69e8a652d76fb00a404
Aftab1999 <ansariaftab9762@gmail.com>
2022-02-14 12:02:36 +0530

    Github users Data

---
+++ src/Api.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import GithubUsers from './githubUsers';
+import Loading from './loading';
+
+
+const Api = () => {
+
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    const getUsers = async () => {
+        try {
+            const response = await fetch('https://api.github.com/users');
+             setLoading(false);
+            setUsers(await response.json());
+        } catch (error) {
+            setLoading(false);
+            console.log("my error is "+ error);
+        }
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    if (loading) {
+        return <Loading />
+    }
+
+    return (
+        <>
+            <GithubUsers users={users}/>
+        </>
+    )
+}
+
+export default Api
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+import './App.css';
+import Api from './Api';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Api />
   </React.StrictMode>,
   document.getElementById('root')
 );
